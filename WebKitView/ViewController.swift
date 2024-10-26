d34a963e3da869a630e5e27c3614cbda15d584a7
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-26 08:03:59 +0200

    Commit all-in 1.1

--- WebKitView/ViewController.swift
+++ WebKitView/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        urlTextField.delegate = self
+        webView.navigationDelegate = self
+        
+        let homePage = "https://www.apple.com"
+        guard let url = URL(string: homePage) else { return }
+        let request = URLRequest(url: url)
+        
+        urlTextField.text = homePage
+        
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true
     }
 
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    
+    @IBAction func forwardButtonAction(_ sender: UIButton) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+}
 
+extension ViewController: UITextFieldDelegate, WKNavigationDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let urlString = textField.text!
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        urlTextField.text = webView.url?.absoluteString
+        
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+    }
 }
 
